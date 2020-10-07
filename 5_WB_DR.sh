a0770a8307ae57f876d811d55c9f37ff8a306960
alruizzo <67523209+alruizzo@users.noreply.github.com>
2020-10-07 15:22:45 +0200

    Update 5_WB_DR.sh

--- 5_WB_DR.sh
+++ 5_WB_DR.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-## A.L.R.R. Aug 14, 2020; September 30, 2020
-## Script to run whole-brain dual regression for each ROI for each participant.
+## A.L.R.R. Aug 14, 2020 / September 30, 2020 / October 7, 2020
+## Script to run whole-brain (WB) dual regression (DR) for each region of interest (ROI) for each participant.
 
-SEARCHFOLDER=/mnt/storage/Jessica_Lab/analyses/veni-lmu/participants/
+SEARCHFOLDER=/mnt/storage/Jessica_Lab/analyses/veni-lmu/individual_rois/
 DATAFOLDER=/mnt/storage/Jessica_Lab/data/veni/subjects/
 FUNCFILE=/scan-data/niftis/mb_rest/edited-mb-rest.feat/ICA_AROMA/denoised_func_data_nonaggr.nii.gz
 
 for participant in $SEARCHFOLDER*
 do
   foldername=`basename $participant`
-  echo "Process for $foldername..."
+  echo "Process for $foldername"
   cd $participant
   for mask in ${participant}/*
   do
     if [[ `basename ${mask}` == *".nii.gz" ]]
     then
-      maskname=`basename $mask | sed 's/_std_2mm.nii.gz//'`
-      echo "...Now computing WB-DR of $maskname..."
+      maskname=`basename $mask | sed 's/_.nii.gz//'`
+      echo "Now computing WB-DR of $maskname..."
       dual_regression $mask 0 -1 0 SCA_DR_$maskname $DATAFOLDER/${foldername}/$FUNCFILE
       echo "...done"
     fi
