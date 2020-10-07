8a2399b62d31d7217e30eef64a95fdf9eb88a6c0
alruizzo <67523209+alruizzo@users.noreply.github.com>
2020-10-07 15:02:48 +0200

    Update 4_individual_rois.sh

--- 4_individual_rois.sh
+++ 4_individual_rois.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-## A.L.R.R. July 30, 2020; September 29, 2020
+## A.L.R.R. July 30, 2020 / September 29, 2020 / October 7, 2020
 ## Script to reslice each ROI mask from MNI to the individual functional space
 
 # Specifiy paths
 SEARCHFOLDER=/mnt/storage/Jessica_Lab/data/veni/subjects/
-OUTPUTFOLDER=/mnt/storage/Jessica_Lab/analyses/veni-lmu/participants/
-MASKFOLDER=/mnt/storage/Jessica_Lab/analyses/veni-lmu/masks/functional/rois_combined_selected_std_2mm/
+OUTPUTFOLDER=/mnt/storage/Jessica_Lab/analyses/veni-lmu/individual_rois/
+MASKFOLDER=/mnt/storage/Jessica_Lab/analyses/veni-lmu/masks/functional/rois_glasser_selected_std_2mm/
 
 # Create output directory if it doesn't exist
 mkdir -p $OUTPUTFOLDER
@@ -23,7 +23,7 @@ do
         mkdir $OUTPUTFOLDER/$foldername
         for mask in $MASKFOLDER*
         do
-          maskname=`basename $mask | sed 's/.nii.gz//'`
+          maskname=`basename $mask | sed 's/std_2mm.nii.gz//'`
           echo "...flirt on $maskname..."
           flirt -in $mask -ref $folder/scan-data/niftis/mb_rest/edited-mb-rest.feat/reg/example_func.nii.gz -applyxfm -init $folder/scan-data/niftis/mb_rest/edited-mb-rest.feat/reg/standard2example_func.mat -out $OUTPUTFOLDER/${foldername}/${maskname}.nii.gz
           echo "...done"
@@ -32,11 +32,7 @@ do
   fi
 done
 
-# Delete empty participants' folders
-find /mnt/storage/Jessica_Lab/analyses/veni-lmu/participants/ -type d -empty -delete
+# Delete empty participants' folders, as those are missing data (and won't be used further)
+find $OUTPUTFOLDER -type d -empty -delete
 
-echo "* FINISHED $0. See results in $OUTPUTFOLDER *"
-
-# Notes
-# Path for EPI (*non*-multiband data)
-#$folder/scan-data/niftis/ep2d_rest/unnormalized/fmri.feat/reg/example_func.nii.gz
+echo "* FINISHED $0 on $(date). See results in $OUTPUTFOLDER *"
