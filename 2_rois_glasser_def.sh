23e5880160f94533b596ebfcd1c498cea2f7b9ba
alruizzo <67523209+alruizzo@users.noreply.github.com>
2020-10-07 06:28:01 -0400

    Updated via the Open Science Framework

--- 2_rois_glasser_def.sh
+++ 2_rois_glasser_def.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-## A.L.R.R. September 29, 2020
+## A.L.R.R. September 29, 2020 / October 7, 2020
 ## Script to spatially restrict each Shirer+Glasser ROI mask after visual inspection and review of the Supplementary Neuroanatomical Results of Glasser et al. (2016).
 
 ## ROIs
   ## LIFG -> 264, 266
-  roi1=01-LIFG;
+  roi1=01-LIFG
   area11=264 #(i.e., +180 of those on the RH)
   area21=266
 
@@ -34,43 +34,42 @@
   area25=112
 
 ## Define paths/directories
-INPUTFOLDER=/mnt/storage/Jessica_Lab/analyses/veni-lmu/masks/functional/rois_glasser_std_1mm/
-OUTPUTFOLDER=/mnt/storage/Jessica_Lab/analyses/veni-lmu/masks/functional/rois_combined_selected_std_1mm/
+INPUTFILE=/mnt/storage/Jessica_Lab/analyses/veni-lmu/masks/functional/Glasser/MNI_glasser360MNI_1mm.nii.gz
+OUTPUTFOLDER=/mnt/storage/Jessica_Lab/analyses/veni-lmu/masks/functional/rois_glasser_selected_std_1mm/
 
 ## Create Output dir
 mkdir -p $OUTPUTFOLDER
 
+## Create ROI array to loop for
+ROIS=($roi1 $roi2 $roi3 $roi4 $roi5)
+
 ## Loop to extract the appropriate areas from the Shirer+Glasser maps
-for nii in $INPUTFOLDER*
+for roi in ${ROIS[@]}
 do
-  roiname=`basename $nii | sed 's/_glasser.nii.gz//'`
-  if [[ $roiname == $roi1 ]]
+  if [[ $roi == $roi1 ]]
   then
-    roi=$roi1
     area1=$area11
     area2=$area21
     echo "computing fslmaths for $roi and $area1 and $area2..."
-    fslmaths $nii -thr $area1 -uthr $area1 $OUTPUTFOLDER/temp$area1
-    fslmaths $nii -thr $area2 -uthr $area2 $OUTPUTFOLDER/temp$area2
+    fslmaths $INPUTFILE -thr $area1 -uthr $area1 $OUTPUTFOLDER/temp$area1
+    fslmaths $INPUTFILE -thr $area2 -uthr $area2 $OUTPUTFOLDER/temp$area2
     fslmaths $OUTPUTFOLDER/temp$area1 -add $OUTPUTFOLDER/temp$area2 $OUTPUTFOLDER/temp$roi
     fslmaths $OUTPUTFOLDER/temp$roi -bin $OUTPUTFOLDER/$roi
     rm $OUTPUTFOLDER/temp*
     echo "...done"
-  elif [[ $roiname == $roi2 ]]
+  elif [[ $roi == $roi2 ]]
   then
-    roi=$roi2
     area1=$area12
     area2=$area22
     echo "computing fslmaths for $roi and $area1 and $area2..."
-    fslmaths $nii -thr $area1 -uthr $area1 $OUTPUTFOLDER/temp$area1
-    fslmaths $nii -thr $area2 -uthr $area2 $OUTPUTFOLDER/temp$area2
+    fslmaths $INPUTFILE -thr $area1 -uthr $area1 $OUTPUTFOLDER/temp$area1
+    fslmaths $INPUTFILE -thr $area2 -uthr $area2 $OUTPUTFOLDER/temp$area2
     fslmaths $OUTPUTFOLDER/temp$area1 -add $OUTPUTFOLDER/temp$area2 $OUTPUTFOLDER/temp$roi
     fslmaths $OUTPUTFOLDER/temp$roi -bin $OUTPUTFOLDER/$roi
     rm $OUTPUTFOLDER/temp*
     echo "...done"
-  elif [[ $roiname == $roi3 ]]
+  elif [[ $roi == $roi3 ]]
   then
-    roi=$roi3
     area1=$area13
     area2=$area23
     area3=$area33
@@ -78,43 +77,42 @@ do
     area5=$area53
     area6=$area63
     echo "computing fslmaths for $roi and $area1, $area2, $area3, $area4, $area5, and $area6..."
-    fslmaths $nii -thr $area1 -uthr $area1 $OUTPUTFOLDER/temp$area1
-    fslmaths $nii -thr $area2 -uthr $area2 $OUTPUTFOLDER/temp$area2
-    fslmaths $nii -thr $area3 -uthr $area3 $OUTPUTFOLDER/temp$area3
-    fslmaths $nii -thr $area4 -uthr $area4 $OUTPUTFOLDER/temp$area4
-    fslmaths $nii -thr $area5 -uthr $area5 $OUTPUTFOLDER/temp$area5
-    fslmaths $nii -thr $area6 -uthr $area6 $OUTPUTFOLDER/temp$area6
+    fslmaths $INPUTFILE -thr $area1 -uthr $area1 $OUTPUTFOLDER/temp$area1
+    fslmaths $INPUTFILE -thr $area2 -uthr $area2 $OUTPUTFOLDER/temp$area2
+    fslmaths $INPUTFILE -thr $area3 -uthr $area3 $OUTPUTFOLDER/temp$area3
+    fslmaths $INPUTFILE -thr $area4 -uthr $area4 $OUTPUTFOLDER/temp$area4
+    fslmaths $INPUTFILE -thr $area5 -uthr $area5 $OUTPUTFOLDER/temp$area5
+    fslmaths $INPUTFILE -thr $area6 -uthr $area6 $OUTPUTFOLDER/temp$area6
     fslmaths $OUTPUTFOLDER/temp$area1 -add $OUTPUTFOLDER/temp$area2 -add $OUTPUTFOLDER/temp$area3 -add $OUTPUTFOLDER/temp$area4 -add $OUTPUTFOLDER/temp$area5 -add $OUTPUTFOLDER/temp$area6 $OUTPUTFOLDER/temp$roi
     fslmaths $OUTPUTFOLDER/temp$roi -bin $OUTPUTFOLDER/$roi
     rm $OUTPUTFOLDER/temp*
     echo "...done"
-  elif [[ $roiname == $roi4 ]]
+  elif [[ $roi == $roi4 ]]
   then
-    roi=$roi4
     area1=$area14
     area2=$area24
     echo "computing fslmaths for $roi and $area1 and $area2..."
-    fslmaths $nii -thr $area1 -uthr $area1 $OUTPUTFOLDER/temp$area1
-    fslmaths $nii -thr $area2 -uthr $area2 $OUTPUTFOLDER/temp$area2
+    fslmaths $INPUTFILE -thr $area1 -uthr $area1 $OUTPUTFOLDER/temp$area1
+    fslmaths $INPUTFILE -thr $area2 -uthr $area2 $OUTPUTFOLDER/temp$area2
     fslmaths $OUTPUTFOLDER/temp$area1 -add $OUTPUTFOLDER/temp$area2 $OUTPUTFOLDER/temp$roi
     fslmaths $OUTPUTFOLDER/temp$roi -bin $OUTPUTFOLDER/$roi
     rm $OUTPUTFOLDER/temp*
     echo "...done"
-  elif [[ $roiname == $roi5 ]]
+  elif [[ $roi == $roi5 ]]
   then
-    roi=$roi5
     area1=$area15
     area2=$area25
     echo "computing fslmaths for $roi and $area1 and $area2..."
-    fslmaths $nii -thr $area1 -uthr $area1 $OUTPUTFOLDER/temp$area1
-    fslmaths $nii -thr $area2 -uthr $area2 $OUTPUTFOLDER/temp$area2
+    fslmaths $INPUTFILE -thr $area1 -uthr $area1 $OUTPUTFOLDER/temp$area1
+    fslmaths $INPUTFILE -thr $area2 -uthr $area2 $OUTPUTFOLDER/temp$area2
     fslmaths $OUTPUTFOLDER/temp$area1 -add $OUTPUTFOLDER/temp$area2 $OUTPUTFOLDER/temp$roi
     fslmaths $OUTPUTFOLDER/temp$roi -bin $OUTPUTFOLDER/$roi
     rm $OUTPUTFOLDER/temp*
     echo "...done"
   else
-    echo "All ROIs (except $roiname [cerebellum]) finished. Check results at ----> $OUTPUTFOLDER"
+    echo "All ROIs (except $roi) finished. Check results at ----> $OUTPUTFOLDER"
   fi
 done
 
+echo "${ROIS[@]} finished. Check results at ----> $OUTPUTFOLDER"
 echo "* FINISHED $0 *"
