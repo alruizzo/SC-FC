a3ebcb69b020d815f978e672d7838b12db7f7482
alruizzo <67523209+alruizzo@users.noreply.github.com>
2020-10-09 11:48:29 +0200

    Update 8_fslcc_w_template.sh

--- 8_fslcc_w_template.sh
+++ 8_fslcc_w_template.sh
@@ -1,36 +1,42 @@
 #!/bin/bash
 
 ## A.L.R.R. Aug 18, 2020
-## Script to compare FC maps, derived from each ROI, with the Shirer et al.'s template.
+## October 9, 2020
+
+## Script to compare FC maps, derived from each ROI (in turn, defined from...
+## ...areas taken from the multimodal parcellation of Glasser...
+## ...et al. 2016), with the Shirer et al.'s anterior salience network...
+## ...template.
 
 # Relevant paths/directories or files
-SEARCHFOLDER=/mnt/storage/Jessica_Lab/analyses/veni-lmu/ROI-WB/
-TEMPLATE_ANT_SAL=/mnt/storage/Jessica_Lab/analyses/veni-lmu/masks/functional/Shirer_et_al-anterior_salience/anterior_salience_std.nii.gz
-OUTPUTFILE=/mnt/storage/Jessica_Lab/analyses/veni-lmu/ROI-WB/FSLCC.txt
+SEARCHFOLDER=/mnt/storage/Jessica_Lab/analyses/veni-lmu/roi-wb/
+TEMPLATE_ANT_SAL=/mnt/storage/Jessica_Lab/analyses/veni-lmu/masks/functional/Shirer_et_al-anterior_salience/anterior_salience_std_2mm.nii.gz
+OUTPUTFILE=/mnt/storage/Jessica_Lab/analyses/veni-lmu/roi-wb/1fslcc_w_Shirer.txt
 
-# Loop to compute fslcc
+# Loop to binarize the maps that resulted from randomise (1stt) and compute...
+# ...fslcc for each time point separately
 for ROI in $SEARCHFOLDER*
 do
   if [ -d "$ROI" ]
   then
     roiname=`basename $ROI`
-    echo $roiname
+    echo "ROI: $roiname"
     cd $ROI
     for (( p=1; p<=3; p=p+1 )) #p = timepoints; max: 3; check whether the tfce_corrp files are not binarized; then, binarize them with fslmaths
     do
-      if [ ! -f "${roiname}_0${p}_1stt_tfce_corrp_bin.nii.gz" ]
+      if [ ! -f "${roiname}_0${p}_bin_1stt_tfce_corrp_tstat1.nii.gz" ]
       then
-        echo "binarizing ${roiname}_0${p}_1stt_tfce_corrp_bin.nii.gz"
-        fslmaths ${roiname}_${i}_1stt_tfce_corrp.nii.gz -thr 0.95 -bin ${roiname}_01_1stt_tfce_corrp_bin.nii.gz
+        echo "Now binarizing ${roiname}_0${p}_1stt_tfce_corrp.nii.gz"
+        fslmaths ${roiname}_0${p}_1stt_tfce_corrp_tstat1.nii.gz -thr 0.95 -bin ${roiname}_0${p}_bin_1stt_tfce_corrp_tstat1.nii.gz
       fi
     done
     for timepoint in ${ROI}/*
     do
-      tpname=`basename $timepoint | sed 's/_tstat1.nii.gz//'`
-      if [[ ${tpname} == *tfce_corrp_bin* ]]
+      tpname=`basename $timepoint | sed 's/_1stt_tfce_corrp_tstat1.nii.gz//'`
+      if [[ ${tpname} == *bin* ]]
       then
-        echo "$tpname is binarized" >> $SEARCHFOLDER/temp1.txt
-          fslcc $TEMPLATE_ANT_SAL $ROI/$tpname >> $SEARCHFOLDER/temp2.txt
+        echo "Timepoint '$tpname' of $roiname is binarized" >> $SEARCHFOLDER/temp1.txt
+        fslcc $TEMPLATE_ANT_SAL $timepoint >> $SEARCHFOLDER/temp2.txt
       fi
     done
   fi
@@ -44,6 +50,7 @@ sed -e 's/^[[:space:]]*//' $SEARCHFOLDER/temp2.txt > $SEARCHFOLDER/temp3.txt
 # Merge files
 paste $SEARCHFOLDER/temp1.txt $SEARCHFOLDER/temp3.txt | column -s $'\t' -t > $OUTPUTFILE
 
-echo "***The '$0' script has finished; go to '$OUTPUTFILE' to see the results***"
+echo "* The '$0' script finished on $(date). \
+Go to '$OUTPUTFILE' to see the results *"
 
 rm $SEARCHFOLDER/temp*
