0c2a16c528ab221e33f6621e3a6fd1053769b054
alruizzo <67523209+alruizzo@users.noreply.github.com>
2020-10-09 16:54:50 +0200

    Update 10_cp_files_R.sh

--- 10_cp_files_R.sh
+++ 10_cp_files_R.sh
@@ -1,31 +1,37 @@
 #!/bin/bash
 
 ## A.L.R.R. Aug 20, 2020
-## Script to select relevant information from the participants' folders and pass on to R for ROI-to-ROI correlation analyses
+## October 9, 2020
 
-# # Script output (for errors), I run it manually in the terminal
+## Script to select relevant information from the participants' folders...
+## ...and pass on to R for ROI-to-ROI correlation analyses
+
+# Script output (for errors), I run it manually in the terminal
 #script /mnt/storage/Jessica_Lab/analyses/veni-lmu/checkfile.txt
 
-# Relevant paths/directories or files
-SEARCHFOLDER=/mnt/storage/Jessica_Lab/analyses/veni-lmu/participants/
-OUTPUTFOLDER=/mnt/storage/Jessica_Lab/analyses/veni-lmu/ROI-TS/
+# Specify the relevant paths/directories or files
+ROOTPATH=/mnt/storage/Jessica_Lab/analyses/veni-lmu/
+SEARCHFOLDER=/mnt/storage/Jessica_Lab/analyses/veni-lmu/individual_rois/
+OUTPUTFOLDER=/mnt/storage/Jessica_Lab/analyses/veni-lmu/roi-ts/
 TEMPFOLDER=/mnt/storage/Jessica_Lab/analyses/veni-lmu/temp/
 
 mkdir -p $TEMPFOLDER
 mkdir -p $OUTPUTFOLDER
 
-# Loop to copy participants' files needed only
+# Loop to copy in a temporal folder the participants' files needed
 for participant in $SEARCHFOLDER*
 do
     foldername=`basename $participant`
-    echo $foldername
+    echo "Checking number of files for $foldername..."
     mkdir -p $TEMPFOLDER/$foldername
-    cp $participant/wm*.txt $TEMPFOLDER/$foldername #to compress all content in this folder, if wanted
+    cp $participant/wm*.txt $TEMPFOLDER/$foldername
     cd $TEMPFOLDER/$foldername | echo `ls | wc -l`
+    echo "...done"
 done
 
-# Loop to create one text file per participants (summary)
-for participant in $TEMPFOLDER*
+# Loop to create one text file per participant (summary)
+echo "Now creating one file per participant"
+for participant in $TEMPFOLDER/*
 do
   particname=`basename $participant`
   cd $participant
@@ -33,16 +39,22 @@ do
   do
     filename=`basename $file`
     (echo $filename; cat $file) > ${filename}_temp.txt
+    echo "moving files..."
     mv ${filename}_temp.txt $file
+    echo "...done"
   done
-paste $participant/wmeants_0*.txt $participant/wmeants_WM.txt $participant/wmeants_CSF.txt > $OUTPUTFOLDER/$particname.txt
+paste $participant/wmeants_0*.txt > $OUTPUTFOLDER/$particname.txt
 done
 
 # Compress folder to upload to R
+echo "Now compressing the files..."
 #zip -r /mnt/storage/Jessica_Lab/analyses/veni-lmu/weightedmeantimeseries_folders.zip $TEMPFOLDER #uncomment if you want the zip file including divided into folders
-zip -r /mnt/storage/Jessica_Lab/analyses/veni-lmu/weightedmeantimeseries.zip $OUTPUTFOLDER #summarized in text file
+zip -r $ROOTPATH/weightedmeantimeseries.zip $OUTPUTFOLDER/* #summarized in text file
+echo "...done"
 rm -r $TEMPFOLDER
 
 # End message
-echo "finished the `basename $0` script; see '$OUTPUTFOLDER' to check results"
-#exit #<-to save the output to the text file!
+echo "* FINISHED $0 on $(date) \
+see '$OUTPUTFOLDER' to check results *"
+#exit #<-don't forger. Necessary to save the output of the 'script' command...
+#...to the text file!
