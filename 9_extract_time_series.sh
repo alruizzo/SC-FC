ad85fab3263a25664b89cd2769b7c3a575bf27f1
alruizzo <67523209+alruizzo@users.noreply.github.com>
2020-10-09 15:53:13 +0200

    Update 9_extract_time_series.sh

--- 9_extract_time_series.sh
+++ 9_extract_time_series.sh
@@ -1,27 +1,36 @@
 #!/bin/bash
 
-## A.L.R.R. Aug 5th, 2020; September 30th, 2020
+## A.L.R.R. Aug 5th, 2020
+## September 30th, 2020
+## October 9, 2020
+
 ## Script to extract time series from ROIs for whole-brain analysis.
 
-SEARCHFOLDER=/mnt/storage/Jessica_Lab/analyses/veni-lmu/participants/
+  # Set relevant paths
+SEARCHFOLDER=/mnt/storage/Jessica_Lab/analyses/veni-lmu/individual_rois/
 DATAFOLDER=/mnt/storage/Jessica_Lab/data/veni/subjects/
 FUNCFILE=/scan-data/niftis/mb_rest/edited-mb-rest.feat/ICA_AROMA/denoised_func_data_nonaggr.nii.gz
 
+  # Loop across participants' folders to extract the mean time series...
+  # ...(fslmeants) using each of the ROIs (in native space) as masks. ...
+  # ...The mean is weighed by the intensity value of the voxel within...
+  # ...the mask
 for participant in $SEARCHFOLDER*
 do
   foldername=`basename $participant`
-  echo "Process for $foldername..."
+  echo "Process for $foldername:"
   cd $participant
   for mask in ${participant}/*
   do
-    if [[ `basename ${mask} == *"nii.gz" ]]
+    if [[ `basename ${mask}` == *"nii.gz" ]]
     then
-      maskname=`basename $mask | sed 's/_std_2mm.nii.gz//'`
-      echo "...Now extracting time series of $maskname..."
+      maskname=`basename $mask | sed 's/.nii.gz//'`
+      echo "Now extracting time series of $maskname..."
       fslmeants -i $DATAFOLDER/${foldername}/$FUNCFILE -o $participant/wmeants_${maskname}.txt -m ${mask} -w
+      echo "...done"
     fi
   done
   echo "...Time series extraction: done"
 done
 
-echo "* FINISHED $0 on $(date)*"
+echo "* FINISHED $0 on $(date) *"
