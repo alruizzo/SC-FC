e632a8a3521148094d559d88d674cfc46b153e6b
alruizzo <67523209+alruizzo@users.noreply.github.com>
2020-11-12 14:49:27 +0100

    Update 6_check_file_nr.sh

--- 6_check_file_nr.sh
+++ 6_check_file_nr.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-## A.L.R.R. October 8, 2020
+# A.L.R.R. October 8, 2020
 
-## Script to check which participants' folders have fewer files than they...
-## ..should. Step after WB-DR
+# Script to check which participants' folders have fewer files than they...
+# ..should. Step after WB-DR (SCA folders within each participant's)
 
-  # Define paths
-SEARCHFOLDER=/mnt/storage/Jessica_Lab/analyses/veni-lmu/individual_rois/
-OUTPUTFILE=/mnt/storage/Jessica_Lab/analyses/veni-lmu/checkfiles.txt
+## Define paths
+source var_names.sh
 
-  # Loop through the participants' folder to warp the native whole-brain...
-  #...images to MNI space to prepare them for randomise
+## Loop through the participants' folder to warp the native whole-brain...
+## ...images to MNI space to prepare them for randomise
 for participant in $SEARCHFOLDER*
 do
   foldername=`basename $participant`
@@ -18,17 +17,18 @@ do
   for mask in ${participant}/*
   do
     maskname=`basename $mask`
-    if [[ ${maskname} == SCA* ]]
+    if [[ ${maskname} == SCA* ]] # To constrain to the just created folder
     then
       cd $mask
       nrfiles=`ls | wc -l`
-      if [ $nrfiles -lt 8 ]
+      if [ $nrfiles -lt 7 ] # Adjust number according to what's expected
       then
         echo "For $foldername:" >> $OUTPUTFILE
-        echo "$maskname folder *only* has $nrfiles file" >> $OUTPUTFILE
+        echo "$maskname folder *only* has $nrfiles file(s)" >> $OUTPUTFILE
       fi
     fi
   done
 done
 
+## End message
 echo "* FINISHED '$0' on $(date). Results in $OUTPUTFILE * "
