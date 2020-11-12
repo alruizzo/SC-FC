9e0186cc627cdde69cde3895018c1e437ff1daae
alruizzo <67523209+alruizzo@users.noreply.github.com>
2020-11-12 14:49:24 +0100

    Update 4_individual_rois.sh

--- 4_individual_rois.sh
+++ 4_individual_rois.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-## A.L.R.R. July 30, 2020 / September 29, 2020 / October 7, 2020
-## Script to reslice each ROI mask from MNI to the individual functional space
+# A.L.R.R. July 30, 2020 / September 29, 2020 / October 7, 2020
+# Script to reslice each ROI mask from MNI to the individual functional space
 
-# Specifiy paths
-SEARCHFOLDER=/mnt/storage/Jessica_Lab/data/veni/subjects/
-OUTPUTFOLDER=/mnt/storage/Jessica_Lab/analyses/veni-lmu/individual_rois/
-MASKFOLDER=/mnt/storage/Jessica_Lab/analyses/veni-lmu/masks/functional/rois_glasser_selected_std_2mm/
+## Specifiy paths
+source var_names.sh
 
-# Create output directory if it doesn't exist
+## Create output directory if it doesn't exist
 mkdir -p $OUTPUTFOLDER
 
-# Loop to go through each participant's folder and reslice the mask files to each participant's functional native space
+## Loop to go through each participant's folder and reslice the mask files...
+## ...to each participant's functional native space
 for folder in $SEARCHFOLDER*
 do
   foldername=`basename $folder`
@@ -25,14 +24,16 @@ do
         do
           maskname=`basename $mask | sed 's/_std_2mm.nii.gz//'`
           echo "...flirt on $maskname..."
-          flirt -in $mask -ref $folder/scan-data/niftis/mb_rest/edited-mb-rest.feat/reg/example_func.nii.gz -applyxfm -init $folder/scan-data/niftis/mb_rest/edited-mb-rest.feat/reg/standard2example_func.mat -out $OUTPUTFOLDER/${foldername}/${maskname}.nii.gz
+          flirt -in $mask -ref $folder/${REGFOLDER}/example_func.nii.gz -applyxfm -init $folder/${REGFOLDER}/standard2example_func.mat -out $OUTPUTFOLDER/${foldername}/${maskname}.nii.gz
           echo "...done"
         done
       fi
   fi
 done
 
-# Delete empty participants' folders, as those are missing data (and won't be used further)
+## Delete empty participants' folders, as those are missing data...
+## ...(and won't be used further)
 find $OUTPUTFOLDER -type d -empty -delete
 
+## End
 echo "* FINISHED $0 on $(date). See results in $OUTPUTFOLDER *"
