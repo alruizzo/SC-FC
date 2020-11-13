c0841c96ed61a02ad20cc7e7022b83ed07edec09
alruizzo <67523209+alruizzo@users.noreply.github.com>
2020-11-13 13:34:32 +0100

    Update 9_extract_time_series.sh

--- 9_extract_time_series.sh
+++ 9_extract_time_series.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
-## A.L.R.R. Aug 5th, 2020
-## September 30th, 2020
-## October 9, 2020
+# A.L.R.R. Aug 5, 2020
+# September 30, 2020
+# October 9, 2020
 
-## Script to extract time series from ROIs for whole-brain analysis.
+# Script to extract time series from ROIs for whole-brain analysis.
 
-  # Set relevant paths
-SEARCHFOLDER=/mnt/storage/Jessica_Lab/analyses/veni-lmu/individual_rois/
-DATAFOLDER=/mnt/storage/Jessica_Lab/data/veni/subjects/
-FUNCFILE=/scan-data/niftis/mb_rest/edited-mb-rest.feat/ICA_AROMA/denoised_func_data_nonaggr.nii.gz
+  ## Set relevant paths
+source var_names.sh
 
-  # Loop across participants' folders to extract the mean time series...
-  # ...(fslmeants) using each of the ROIs (in native space) as masks. ...
-  # ...The mean is weighed by the intensity value of the voxel within...
-  # ...the mask
+  ## Loop across participants' folders to extract the mean time series...
+  ## ...(fslmeants) using each of the ROIs (in native space) as masks. ...
+  ## ...The mean is weighed by the intensity value of the voxel within...
+  ## ...the mask. This process takes quite a while (though less than...
+  ## ...randomise)
 for participant in $SEARCHFOLDER*
 do
   foldername=`basename $participant`
@@ -33,4 +32,5 @@ do
   echo "...Time series extraction: done"
 done
 
+  ## End message
 echo "* FINISHED $0 on $(date) *"
